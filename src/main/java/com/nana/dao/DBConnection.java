b178e8c02007156f608bba94280db34bd2b0043d
Amr Mustafa <amrmostafa0366@gmail.com>
2023-06-12 06:17:23 +0300

    DBConnection -- Singelton ThreadSafe DesignPattern

--- src/main/java/com/nana/dao/DBConnection.java
+++ src/main/java/com/nana/dao/DBConnection.java
@@ -5,11 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    //host
-    //port
-    //db_name
-    //username
-    //password
     private static final String HOST = "127.0.0.1";
     private static final int PORT = 3307;
     private static final String DB_NAME = "banko_db";
@@ -17,13 +12,33 @@ public class DBConnection {
     private static final String PASSWORD = "";
     private static Connection connection;
 
-    public static Connection getConnection(){
-        try{
-            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s:%d/%s",HOST,PORT,DB_NAME),USERNAME,PASSWORD);
+    private DBConnection() {
+        try {
+            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s:%d/%s", HOST, PORT, DB_NAME),
+                    USERNAME, PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            synchronized (DBConnection.class) {
+                if (connection == null) {
+                    new DBConnection();
+                }
+            }
         }
         return connection;
     }
 
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
